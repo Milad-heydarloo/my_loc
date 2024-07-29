a40b4579c43231f8ea663e6307da43e2e5b2a69f
milad <78633834+Milad-heydarloo@users.noreply.github.com>
2024-07-29 18:33:04 +0330

    chans

---
+++ example/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:location_picker_flutter_map/location_picker_flutter_map.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Location Picker',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Location Picker'),
+        ),
+        body: FlutterLocationPicker(
+          initZoom: 11,
+          minZoomLevel: 5,
+          maxZoomLevel: 16,
+          trackMyPosition: true,
+          searchBarBackgroundColor: Colors.white,
+          selectedLocationButtonTextstyle: const TextStyle(fontSize: 18),
+          mapLanguage: 'en',
+          onError: (e) => print(e),
+          selectLocationButtonLeadingIcon: const Icon(Icons.check),
+          onPicked: (pickedData) {
+            print(pickedData.latLong.latitude);
+            print(pickedData.latLong.longitude);
+            print(pickedData.address);
+            print(pickedData.addressData);
+          },
+          onChanged: (pickedData) {
+            print(pickedData.latLong.latitude);
+            print(pickedData.latLong.longitude);
+            print(pickedData.address);
+            print(pickedData.addressData);
+          },
+          showContributorBadgeForOSM: true,
+        ),
+      ),
+    );
+  }
+}
