00fd1a3ca87a7956645e7d0760101bed8bc91202
milad <78633834+Milad-heydarloo@users.noreply.github.com>
2024-08-03 20:18:07 +0330

    سیبدسبلسد

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,20 +1,13 @@
 import UIKit
 import Flutter
-import CoreLocation
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  let locationService = LocationService.shared
-
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-
-    // Start updating location
-    locationService.startUpdatingLocation()
-
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
